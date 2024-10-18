b354d49d91f07e4496e2bcbfc878a356a1d0eb8e
Josenilto L Silva <josenilto@outlook.com>
2024-10-18 02:21:06 -0300

    Atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,12 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        sections = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav__menu a[href*=' + sections + ']').classList.add('active-link')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav__menu a[href*=' + sections + ']').classList.remove('active-link')
         }
     })
 }
